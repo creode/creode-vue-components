1d1025546503a495adc7a582e9669f600a9fbf31
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-02-09 16:23:15 +0000

    Initial storybook configuration.

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/vue3-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack"
+  ],
+  framework: {
+    name: "@storybook/vue3-webpack5",
+    options: {
+      builder: {
+        useSWC: true,
+      },
+    },
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
