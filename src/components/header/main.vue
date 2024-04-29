018ee1e454fa759501ade74ad0d430f8a3b32002
Ben Leonard <ben.leonard@creode.co.uk>
2024-04-29 12:01:23 +0100

    Header keyboard accessibility improvements.

--- src/components/header/main.vue
+++ src/components/header/main.vue
@@ -9,13 +9,13 @@
 				</div>
 				<div class="header__section header__section--menu" :class="{ 'header__section--desktop-menu': isDesktopScreen() }" v-if="$slots.menu">
 					<div class="header__menu-toggle-wrapper" :hidden="isDesktopScreen()">
-						<input id="header-menu-toggle" type="checkbox" class="header__menu-toggle" v-model="menuExpanded" hidden>
+						<input id="header-menu-toggle" type="checkbox" class="header__menu-toggle" v-model="menuExpanded" v-on:keyup.enter="menuToggleOnEnter" hidden>
 						<label for="header-menu-toggle" class="header__menu-toggle-label" role="button" aria-haspopup="menu" :aria-expanded="menuExpanded" aria-controls="header-menu">
 							{{ menuExpanded ? 'Close menu' : 'Open menu' }}
 						</label>
 					</div>
 					<div id="header-menu" class="header__menu-wrapper" :hidden="!menuExpanded">
-						<nav class="header__menu-inner">
+						<nav class="header__menu-inner" ref="menuInner">
 							<div class="header__menu-close-button-wrapper" :hidden="isDesktopScreen()">
 								<button type="button" class="header__menu-close-button" v-on:click="menuExpanded = false">
 									Close menu
@@ -51,13 +51,32 @@
 					this.resetMenuExpandedState();
 				}
 			);
+			this.setFocusableMenuItems();
 		},
 		methods: {
+			// General methods.
 			resetMenuExpandedState() {
 				this.menuExpanded = this.isDesktopScreen();
 			},
 			isDesktopScreen() {
 				return this.breakpoint < window.innerWidth;
+			},
+			setFocusableMenuItems() {
+				let focusables = this.$refs.menuInner.querySelectorAll('a, button');
+
+				for (let i = 0; i < focusables.length; i++) {
+					focusables[i].tabIndex = this.menuExpanded ? 0 : -1;
+				}
+			},
+			// Event handlers.
+			menuToggleOnEnter(event) {
+				event.preventDefault();
+				this.menuExpanded = ! this.menuExpanded;
+			}
+		},
+		watch: {
+			menuExpanded() {
+				this.setFocusableMenuItems();
 			}
 		}
 	};
