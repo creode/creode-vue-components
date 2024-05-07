15eefd5ba39b644aabf1540ce4ae00b98777a7d0
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-05-07 16:13:06 +0100

    Adds in a tabbed content component.

---
+++ src/components/tabbed-content/sub-components/content.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-show="isVisible">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { inject, computed } from 'vue';
+
+export default {
+  name: 'tab-content-item',
+  props: {
+    index: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const openItemIndex = inject('openItemIndex');
+
+    const isVisible = computed(() => {
+      return props.index === openItemIndex.value;
+    });
+
+    return {
+      isVisible
+    };
+  }
+};
+</script>
