a38edc0407242b6579aed544e928cb524d3fe861
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-05-29 16:01:44 +0100

    Split up the CSS into smaller sections and move the component into the overlay.

--- src/components/modal/main.vue
+++ src/components/modal/main.vue
@@ -5,17 +5,16 @@
         <slot name="trigger-content"></slot>
       </a>
     </div>
-    <Teleport :to="modalLocation" v-if="isMounted">
-      <Transition name="model-content">
-        <div class="modal__content" v-if="modalOpen">
-          <span class="modal__close" @click="modalOpen = false"><slot name="close-icon">X</slot></span>
-          <slot></slot>
-        </div>
-      </Transition>
-    </Teleport>
     <Teleport :to="overlayLocation" v-if="isMounted">
-      <Transition name="fade">
-        <div class="modal__overlay" v-if="modalOpen" @click="modalOpen = false"></div>
+      <Transition name="model-overlay">
+        <div class="modal__overlay" v-if="modalOpen" @click="modalOpen = false">
+          <div class="modal__content" v-if="modalOpen">
+            <div class="modal__close-wrapper">
+              <button class="modal__close" @click.prevent="modalOpen = false"><slot name="close-icon">X</slot></button>
+            </div>
+            <slot></slot>
+          </div>
+        </div>
       </Transition>
     </Teleport>
   </div>
