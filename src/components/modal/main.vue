d93de708ccee6465cc48df15a4b483028c36e94b
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-05-29 15:17:24 +0100

    Adds in basic modal component.

---
+++ src/components/modal/main.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="modal">
+    <div class="modal__trigger">
+      <a href="#" @click.prevent="modalOpen = !modalOpen">
+        <slot name="trigger-content"></slot>
+      </a>
+    </div>
+    <Teleport :to="modalLocation" v-if="isMounted">
+      <Transition name="model-content">
+        <div class="modal__content" v-if="modalOpen">
+          <span class="modal__close" @click="modalOpen = false"><slot name="close-icon">X</slot></span>
+          <slot></slot>
+        </div>
+      </Transition>
+    </Teleport>
+    <Teleport :to="overlayLocation" v-if="isMounted">
+      <Transition name="fade">
+        <div class="modal__overlay" v-if="modalOpen" @click="modalOpen = false"></div>
+      </Transition>
+    </Teleport>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'modal',
+  props: {
+    modalLocation: {
+      type: String,
+      default: 'body'
+    },
+    overlayLocation: {
+      type: String,
+      default: 'body'
+    }
+  },
+  data() {
+    return {
+      modalOpen: false,
+      isMounted: false
+    }
+  },
+  mounted(){
+    this.isMounted = true;
+  }
+}
+</script>
